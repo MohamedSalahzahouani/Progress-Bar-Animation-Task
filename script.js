922ec6a7e35f939e68599d07cccd99095620f022
MohamedSalahzahouani <salahzahouani25@gmail.com>
2024-09-11 17:02:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // ADD your logic here
+document.getElementById('startBtn').addEventListener('click', function() {
+    const progressBar = document.getElementById('progress-bar');
+    const duration=3000; // Animation duration in 3000milliseconds (3 seconds):30000ms=3s
+    const intervalTime = 10;
+    const totalduration=(duration/intervalTime);
+    const progressBarContainer = document.createElement("div");
+    const progressBar = document.createElement("span");
+    progressBarContainer.appendChild(progressBar);
+}
+
+  
+    
